f08ee75bbf8d9af11cfd3812082207e12b033de3
Дмитрий Коптелов <koptel007@yandex.ru>
2019-12-10 15:16:34 +0300

    Исправляет замечания наставника по неактивнÐвным ссылÑкам в footerÐ, не рабочей карте Google, проблемы с сетками

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,10 @@
 var mainNav = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var form = document.querySelector('.form');
-var weight = form.querySelector('[name=weight]');
-var user = form.querySelector('[name=name]');
-var email = form.querySelector('[name=email]');
-var emailSvg = form.querySelector('.form__mail');
-var phone = form.querySelector('[name=phone]');
 
 mainNav.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function () {
   if (mainNav.classList.contains('main-nav--closed')) {
     mainNav.classList.remove('main-nav--closed');
     mainNav.classList.add('main-nav--opened');
@@ -19,80 +14,89 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-user.focus();
+if (form) {
+  var weight = form.querySelector('[name=weight]');
+  var user = form.querySelector('[name=name]');
+  var email = form.querySelector('[name=email]');
+  var emailSvg = form.querySelector('.form__mail');
+  var phone = form.querySelector('[name=phone]');
 
-form.addEventListener("submit", function (evt) {
-  if (!user.value) {
-    evt.preventDefault("");
-    console.log("Нужно заполнить форму!!!");
-    user.offsetWidth = user.offsetWidth;
-    user.classList.add("form__input--invalid");
-    user.focus();
-  } else {
-    user.classList.remove("form__input--invalid");
-    weight.focus();
-  }
-});
+  user.focus();
 
-form.addEventListener("submit", function (evt) {
-  if (!weight.value || isNaN(weight.value)) {
-    evt.preventDefault("");
-    console.log("Нужно заполнить форму!!!");
-    weight.offsetWidth = weight.offsetWidth;
-    weight.classList.add("form__input--invalid");
-  } else {
-
-    weight.classList.remove("form__input--invalid");
+  form.addEventListener("submit", function (evt) {
     if (!user.value) {
+      evt.preventDefault("");
+      console.log("Нужно заполнить форму!!!");
+      user.offsetWidth = user.offsetWidth;
+      user.classList.add("form__input--invalid");
       user.focus();
     } else {
-      email.focus();
-    }
-  }
-});
-
-form.addEventListener("submit", function (evt) {
-  if (!email.value) {
-    evt.preventDefault("");
-    console.log("Нужно заполнить форму!!!");
-    email.offsetWidth = email.offsetWidth;
-    email.classList.add("form__input--invalid");
-    emailSvg.classList.add("form__mail--invalid");
-  } else {
-    email.classList.remove("form__input--invalid");
-    emailSvg.classList.remove("form__mail--invalid");
-
-    if (!user.value) {
-      user.focus();
-    }
-
-    if (!weight.value) {
+      user.classList.remove("form__input--invalid");
       weight.focus();
-    } else {
-      phone.focus();
     }
-  }
-});
+  });
 
-form.addEventListener("submit", function (evt) {
-  if (!phone.value) {
-    evt.preventDefault("");
-    console.log("Нужно заполнить форму!!!");
-    phone.offsetWidth = phone.offsetWidth;
-    phone.classList.add("form__input--invalid");
-  } else {
-    phone.classList.remove("form__input--invalid");
-
-  if (!user.value) {
-      user.focus();
+  form.addEventListener("submit", function (evt) {
+    if (!weight.value || isNaN(weight.value)) {
+      evt.preventDefault("");
+      console.log("Нужно заполнить форму!!!");
+      weight.offsetWidth = weight.offsetWidth;
+      weight.classList.add("form__input--invalid");
     } else {
+
+      weight.classList.remove("form__input--invalid");
+      if (!user.value) {
+        user.focus();
+      } else {
+        email.focus();
+      }
+    }
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!email.value) {
+      evt.preventDefault("");
+      console.log("Нужно заполнить форму!!!");
+      email.offsetWidth = email.offsetWidth;
+      email.classList.add("form__input--invalid");
+      emailSvg.classList.add("form__mail--invalid");
+    } else {
+      email.classList.remove("form__input--invalid");
+      emailSvg.classList.remove("form__mail--invalid");
+
+      if (!user.value) {
+        user.focus();
+      }
+
       if (!weight.value) {
         weight.focus();
       } else {
-        if (!email.value) {
-          email.focus();
+        phone.focus();
+      }
+    }
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!phone.value) {
+      evt.preventDefault("");
+      console.log("Нужно заполнить форму!!!");
+      phone.offsetWidth = phone.offsetWidth;
+      phone.classList.add("form__input--invalid");
+    } else {
+      phone.classList.remove("form__input--invalid");
+
+      if (!user.value) {
+        user.focus();
+      } else {
+        if (!weight.value || isNaN(weight.value)) {
+          weight.focus();
+        } else {
+          if (!email.value) {
+            email.focus();
+          }
         }
       }
     }
-  }
-});
+  })
+}
+;
